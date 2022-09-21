9a0923878803e3e40aca68e20af0c4a9bfd4a0fb
ac <anthonyceja02@gmail.com>
2022-09-21 16:37:14 -0500

    Anthony's push

--- app/src/main/java/com/example/whackathing/GameActivity.kt
+++ app/src/main/java/com/example/whackathing/GameActivity.kt
@@ -3,13 +3,23 @@ package com.example.whackathing
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+
+class Mol(val mole: ImageView) {
+
+    init{
+        var testing: ImageView = mole;
+    }
+
+}
 
 class GameActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
     }
+
 
     fun goToHome(view: View){
         val intent = Intent(this, MainActivity::class.java)
