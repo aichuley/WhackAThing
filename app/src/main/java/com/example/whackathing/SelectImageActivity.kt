96b2dbd988a859a2b0a2409b40ac9be863db9bee
deepikadan <71300079+deepikadan@users.noreply.github.com>
2022-09-22 10:41:23 -0500

    Adding functionality of radio button to change square color

--- app/src/main/java/com/example/whackathing/SelectImageActivity.kt
+++ app/src/main/java/com/example/whackathing/SelectImageActivity.kt
@@ -8,12 +8,17 @@ import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.activity.result.contract.ActivityResultContracts
+
 
 class SelectImageActivity : AppCompatActivity(){
 
     private lateinit var selectImageButton: Button
     private lateinit var previewImage: ImageView
+    private lateinit var colorSquare: ImageView
+    private lateinit var backgroundColor: RadioGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +26,24 @@ class SelectImageActivity : AppCompatActivity(){
 
         selectImageButton = findViewById(R.id.selectImageButton)
         previewImage = findViewById(R.id.previewImage)
+        colorSquare = findViewById(R.id.colorSquare)
+        backgroundColor = findViewById(R.id.backgroundColor)
 
         selectImageButton.setOnClickListener{
             val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
             resultLauncher.launch(intent)
         }
+
+        backgroundColor.setOnCheckedChangeListener {_, checkedId -> val radio: RadioButton? = findViewById(checkedId)
+            val newColor = when(radio?.id) {
+                R.id.white -> R.drawable.rectangle_white
+                R.id.green -> R.drawable.rectangle_green
+                R.id.black -> R.drawable.rectangle_black
+                else -> R.drawable.rectangle_pink
+            }
+            colorSquare.setImageResource(newColor)
+        }
+
 
     }
 
