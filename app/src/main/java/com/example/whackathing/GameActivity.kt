dea958fcdf8a5854b7543e4b2d1aba6c8a1f5abf
ac <anthonyceja02@gmail.com>
2022-09-22 15:58:56 -0500

    Anthony and Ashley push

--- app/src/main/java/com/example/whackathing/GameActivity.kt
+++ app/src/main/java/com/example/whackathing/GameActivity.kt
@@ -1,11 +1,13 @@
 package com.example.whackathing
 
 import android.content.Intent
+import android.media.Image
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 
 class Mol(val mole: ImageView) {
 
@@ -17,7 +19,7 @@ class Mol(val mole: ImageView) {
 
 class GameActivity: AppCompatActivity() {
 
-    private var boardList = mutableListOf<Button>()
+    private var boardList = mutableListOf<ImageView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,15 +30,28 @@ class GameActivity: AppCompatActivity() {
 
     private fun initBoard() {
 
-        boardList.add(0,findViewById<Button>(R.id.a1))
-        boardList.add(1,findViewById<Button>(R.id.a2))
-        boardList.add(2,findViewById<Button>(R.id.a3))
-        boardList.add(3,findViewById<Button>(R.id.b1))
-        boardList.add(4,findViewById<Button>(R.id.b2))
-        boardList.add(5,findViewById<Button>(R.id.b3))
-        boardList.add(6,findViewById<Button>(R.id.c1))
-        boardList.add(7,findViewById<Button>(R.id.c2))
-        boardList.add(8,findViewById<Button>(R.id.c3))
+        boardList.add(0,findViewById<ImageView>(R.id.a1))
+        boardList.add(1,findViewById<ImageView>(R.id.a2))
+        boardList.add(2,findViewById<ImageView>(R.id.a3))
+        boardList.add(3,findViewById<ImageView>(R.id.b1))
+        boardList.add(4,findViewById<ImageView>(R.id.b2))
+        boardList.add(5,findViewById<ImageView>(R.id.b3))
+        boardList.add(6,findViewById<ImageView>(R.id.c1))
+        boardList.add(7,findViewById<ImageView>(R.id.c2))
+        boardList.add(8,findViewById<ImageView>(R.id.c3))
+
+
+        var currImg = boardList.getOrNull(generateRandom())
+
+        currImg!!.setImageResource(R.drawable.mol)
+        currImg!!.setOnClickListener {
+            currImg!!.setImageResource(0);
+        }
+
+    }
+
+    fun generateRandom(): Int{
+        return (0..9).random()
     }
 
     fun goToHome(view: View){
