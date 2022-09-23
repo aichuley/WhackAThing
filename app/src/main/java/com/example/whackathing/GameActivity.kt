109beb01f8110dc1eee2b7e20858b14e53d46f59
ac <anthonyceja02@gmail.com>
2022-09-23 12:51:38 -0500

    Anthonys push

--- app/src/main/java/com/example/whackathing/GameActivity.kt
+++ app/src/main/java/com/example/whackathing/GameActivity.kt
@@ -3,23 +3,45 @@ package com.example.whackathing
 import android.content.Intent
 import android.media.Image
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 
+
 class Mol(val mole: ImageView) {
 
-    init{
-        var testing: ImageView = mole;
+   var timer =  object : CountDownTimer(3000, 1000) {
+
+        override fun onTick(millisUntilFinished: Long) {
+            //mTextField.setText("seconds remaining: " + millisUntilFinished / 1000)
+        }
+
+        override fun onFinish() {
+            //mTextField.setText("done!")
+            mole!!.setImageResource(0)
+        }
+    }.start()
+
+    fun getImage() : ImageView {
+        return mole
+    }
+
+    fun setMole() {
+        mole!!.setImageResource(R.drawable.mol)
+        mole!!.setOnClickListener {
+            mole!!.setImageResource(0)
+        }
     }
 
 }
 
+
 class GameActivity: AppCompatActivity() {
 
-    private var boardList = mutableListOf<ImageView>()
+    private var boardList = mutableListOf<Mol>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,23 +52,55 @@ class GameActivity: AppCompatActivity() {
 
     private fun initBoard() {
 
-        boardList.add(0,findViewById<ImageView>(R.id.a1))
-        boardList.add(1,findViewById<ImageView>(R.id.a2))
-        boardList.add(2,findViewById<ImageView>(R.id.a3))
-        boardList.add(3,findViewById<ImageView>(R.id.b1))
-        boardList.add(4,findViewById<ImageView>(R.id.b2))
-        boardList.add(5,findViewById<ImageView>(R.id.b3))
-        boardList.add(6,findViewById<ImageView>(R.id.c1))
-        boardList.add(7,findViewById<ImageView>(R.id.c2))
-        boardList.add(8,findViewById<ImageView>(R.id.c3))
+        boardList.add(0,Mol(findViewById<ImageView>(R.id.a1)))
+        boardList.add(1,Mol(findViewById<ImageView>(R.id.a2)))
+        boardList.add(2,Mol(findViewById<ImageView>(R.id.a3)))
+        boardList.add(3,Mol(findViewById<ImageView>(R.id.b1)))
+        boardList.add(4,Mol(findViewById<ImageView>(R.id.b2)))
+        boardList.add(5,Mol(findViewById<ImageView>(R.id.b3)))
+        boardList.add(6,Mol(findViewById<ImageView>(R.id.c1)))
+        boardList.add(7,Mol(findViewById<ImageView>(R.id.c2)))
+        boardList.add(8,Mol(findViewById<ImageView>(R.id.c3)))
 
+//        val boardMole = boardList.getOrNull(generateRandom())
+//        boardMole!!.setMole()
 
-        var currImg = boardList.getOrNull(generateRandom())
+//        var timer =  object : CountDownTimer(2000, 1000) {
+//
+//            override fun onTick(millisUntilFinished: Long) {
+//                //mTextField.setText("seconds remaining: " + millisUntilFinished / 1000)
+//            }
+//
+//            override fun onFinish() {
+//                //mTextField.setText("done!")
+//                //!!.setImageResource(0)
+////                val boardMole = boardList.getOrNull(generateRandom())
+////
+////                boardMole!!.setMole()
+//
+//            }
+//        }.start()
 
-        currImg!!.setImageResource(R.drawable.mol)
-        currImg!!.setOnClickListener {
-            currImg!!.setImageResource(0);
+//        for(i in 1..3) {
+//
+//            val boardMole = boardList.getOrNull(generateRandom())
+//            boardMole!!.setMole()
+//
+//        }
+
+        while(true) {
+            val boardMole = boardList.getOrNull(generateRandom())
+            boardMole!!.setMole()
         }
+
+        //add score, if they fail to bit 3 mol break out of the loop
+
+
+
+//        currImg!!.setImageResource(R.drawable.mol)
+//        currImg!!.setOnClickListener {
+//            currImg!!.setImageResource(0)
+//        }
 
     }
 
